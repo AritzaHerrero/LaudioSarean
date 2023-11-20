210b6ae2f521504ea8cc15d5c03c3ca315a1cb0e
Aingeru <aingerusir@gmail.com>
2023-11-20 14:19:20 +0500

    Entity eta Dao sortuta

--- app/src/main/java/com/talde3/laudiosarean/Dao/GuenaDao.java
+++ app/src/main/java/com/talde3/laudiosarean/Dao/GuenaDao.java
@@ -1,7 +1,12 @@
 package com.talde3.laudiosarean.Dao;
 
 import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.talde3.laudiosarean.Entities.Gunea;
 
 @Dao
 public interface GuenaDao {
+    @Query("Select * from Gunea")
+    Gunea getAll();
 }
--- app/src/main/java/com/talde3/laudiosarean/Dao/RecordDao.java
+++ app/src/main/java/com/talde3/laudiosarean/Dao/RecordDao.java
@@ -1,8 +1,12 @@
 package com.talde3.laudiosarean.Dao;
 
 import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.talde3.laudiosarean.Entities.Record;
 
 @Dao
 public interface RecordDao {
-
+    @Query("Select * from Record")
+    Record getAll();
 }
