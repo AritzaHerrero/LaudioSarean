9c6b990efbc907da73d69c4c391246dfd154d003
Aritza <aritz.herreroni@elorrieta-errekamari.com>
2023-11-23 12:08:02 +0100

    Gune infotmazioaren activitian audio sartu eta ImageSlider sartu.

--- app/src/main/java/com/talde3/laudiosarean/GuneInformazioa.java
+++ app/src/main/java/com/talde3/laudiosarean/GuneInformazioa.java
@@ -4,17 +4,25 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+
+import androidx.viewpager.widget.ViewPager;
 
 import java.io.IOException;
 
 public class GuneInformazioa extends Activity {
-    private Button btnHasi;
-    private Button btnGelditu;
-    private Button btnBerezarri;
+    private Handler handler = new Handler();
+    private Runnable runnable;
+    private ImageButton imgBtnHasi;
+    private ImageButton imgBtnGelditu;
+    private ImageButton imgBtnBerrebiarazi;
     private MediaPlayer mediaPlayer;
+    private SeekBar seekBarAudio;
     private boolean isPlaying = false;
+    private int audioarenPosizioa = 0;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,28 +32,68 @@ public class GuneInformazioa extends Activity {
         setContentView(R.layout.activity_gune_informazioa);
 
 
+        ViewPager viewPager = findViewById(R.id.imageSlider);
+        int[] images = {R.drawable.berrebiarazi, R.drawable.hasi, R.drawable.gelditu};
+        ImageSliderAdapter adapter = new ImageSliderAdapter(this, images);
+        viewPager.setAdapter(adapter);
+
+
         // MediaPlayer klasea abiaraziko dugu Audio fitxategia elkartuz
         mediaPlayer = MediaPlayer.create(this, R.raw.audioa4);
-
         // Interfazearen hiru botoiak lortuko ditugu
-        btnHasi = findViewById(R.id.btnHasi);
-        btnGelditu = findViewById(R.id.btnGelditu);
-        btnBerezarri = findViewById(R.id.btnBerrabiarazi);
+        imgBtnHasi = findViewById(R.id.imgBtnHasi);
+        imgBtnGelditu = findViewById(R.id.imgBtnGelditu);
+        imgBtnBerrebiarazi = findViewById(R.id.imgBtnBerrebiarazi);
+        seekBarAudio= findViewById(R.id.seekBarAudio);
+
+        imgBtnHasi.setVisibility(View.VISIBLE);
+        imgBtnGelditu.setVisibility(View.GONE);
+
+
+        //seekBar max jarri
+        seekBarAudio.setMax(mediaPlayer.getDuration());
+
+        // Actualizar la posición del SeekBar durante la reproducción del audio
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (mediaPlayer != null) {
+                    int mCurrentPosition = mediaPlayer.getCurrentPosition();
+                    seekBarAudio.setProgress(mCurrentPosition);
+                }
+                handler.postDelayed(this, 100);
+            }
+        };
+        handler.postDelayed(runnable, 100);
+
+        seekBarAudio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress); // Aldatu audioaren posizioa SeekBar-en aurrerapenera
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
 
         // Konfiguratu botoiaren klik erreprodukzioa hasteko edo eteteko
-        btnHasi.setOnClickListener(new View.OnClickListener() {
+        imgBtnHasi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isPlaying) {
                     startAudio();
-                } else {
-                    pauseAudio();
                 }
             }
         });
 
         // Konfiguratu botoiaren klik erreprodukzioa gelditzeko
-        btnGelditu.setOnClickListener(new View.OnClickListener() {
+        imgBtnGelditu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopAudio();
@@ -53,7 +101,7 @@ public class GuneInformazioa extends Activity {
         });
 
         // Konfiguratu botoiaren klik, erreprodukzioa hasieratik berriz hasteko
-        btnBerezarri.setOnClickListener(new View.OnClickListener() {
+        imgBtnBerrebiarazi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 restartAudio();
@@ -64,27 +112,27 @@ public class GuneInformazioa extends Activity {
     // Audioa erreproduzitzen hasteko edo berriz hasteko metodoa
     private void startAudio() {
         if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.seekTo(seekBarAudio.getProgress());
             mediaPlayer.start();
             isPlaying = true;
-            btnHasi.setText("Gelditu");
-        }
-    }
-
-    // Audio-erreprodukzioa eteteko metodoa
-    private void pauseAudio() {
-        if (mediaPlayer.isPlaying()) {
-            mediaPlayer.pause();
+            imgBtnHasi.setVisibility(View.GONE);
+            imgBtnGelditu.setVisibility(View.VISIBLE);
+        } else {
             isPlaying = false;
-            btnHasi.setText("Jarraitu");
+            mediaPlayer.pause();
+            imgBtnHasi.setVisibility(View.VISIBLE);
+            imgBtnGelditu.setVisibility(View.GONE);
         }
     }
 
     // Audio-erreprodukzioa gelditzeko metodoa
     private void stopAudio() {
         if (mediaPlayer.isPlaying()) {
+            audioarenPosizioa = mediaPlayer.getCurrentPosition();
             mediaPlayer.stop();
             isPlaying = false;
-            btnHasi.setText("Erreproduzitu");
+            imgBtnHasi.setVisibility(View.VISIBLE);
+            imgBtnGelditu.setVisibility(View.GONE);
             try {
                 mediaPlayer.prepare();
             } catch (IOException e) {
@@ -98,7 +146,8 @@ public class GuneInformazioa extends Activity {
         mediaPlayer.seekTo(0);
         mediaPlayer.start();
         isPlaying = true;
-        btnHasi.setText("Gelditu");
+        imgBtnHasi.setVisibility(View.GONE);
+        imgBtnGelditu.setVisibility(View.VISIBLE);
     }
 
     @Override
---
+++ app/src/main/java/com/talde3/laudiosarean/ImageSliderAdapter.java
@@ -0,0 +1,46 @@
+package com.talde3.laudiosarean;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageSliderAdapter extends PagerAdapter {
+
+    private Context context;
+    private int[] images;
+
+    public ImageSliderAdapter(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.activity_image_slider_adapter, container, false);
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
