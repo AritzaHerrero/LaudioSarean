9f2b0c4eaa0824d7ff2ed7283d4fe2c1f03239c9
Unax Zulaika Fuente <markel.seabrookesbi@elorrieta-errekamari.com>
2023-11-27 12:33:24 +0100

    Guneen argazki guztiak ipinita, eta Yermoko Andre Mariaren Santutegiak bere aktibitateari eramaten du.

--- app/src/main/java/com/talde3/laudiosarean/GuneakFragment.java
+++ app/src/main/java/com/talde3/laudiosarean/GuneakFragment.java
@@ -1,5 +1,6 @@
 package com.talde3.laudiosarean;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,8 @@ public class GuneakFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private ImageButton ibSantutegia;
 
     public GuneakFragment() {
         // Required empty public constructor
@@ -56,9 +60,19 @@ public class GuneakFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guneak, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_guneak, container, false);
+
+        ImageButton ibSantutegia = view.findViewById(R.id.ibSantutegia); // Reemplaza "tuImageButtonId" con el ID real de tu ImageButton
+
+        ibSantutegia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent YermokoSantutegiaIntent = new Intent(getActivity(), GuneInformazioa.class);
+                startActivity(YermokoSantutegiaIntent);
+            }
+        });
+
+        return view;
     }
 }
