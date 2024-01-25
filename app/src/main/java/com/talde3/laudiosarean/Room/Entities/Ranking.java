057d08c217da70c61eec338c1c6b85b862c0a6cb
“Aingeru” <aingerudaniel.siranaulasa@elorrieta-errekamari.com>
2024-01-25 19:35:46 +0100

    Ranking izena eta abizena, spinner guneen izena eta zenbakia

--- app/src/main/java/com/talde3/laudiosarean/Room/Entities/Ranking.java
+++ app/src/main/java/com/talde3/laudiosarean/Room/Entities/Ranking.java
@@ -3,10 +3,12 @@ package com.talde3.laudiosarean.Room.Entities;
 public class Ranking {
     private String izena;
     private int puntuazioa;
+    private String abizenak;
 
     // Constructor
-    public Ranking(String izena, int puntuazioa) {
+    public Ranking(String izena, int puntuazioa, String abizenak) {
         this.izena = izena;
+        this.abizenak = abizenak;
         this.puntuazioa = puntuazioa;
     }
 
@@ -17,6 +19,7 @@ public class Ranking {
     public int getPuntuazioa() {
         return puntuazioa;
     }
+    public String getAbizenak() { return abizenak; }
 
     // Setters
     public void setIzena(String izena) {
@@ -25,4 +28,5 @@ public class Ranking {
     public void setPuntuazioa(int puntuazioa) {
         this.puntuazioa = puntuazioa;
     }
+    public void setAbizenak(String abizenak) { this.abizenak = abizenak; }
 }
